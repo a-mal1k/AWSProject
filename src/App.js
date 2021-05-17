f2006aa4ae5234be6e8773c9592ad4df6508e5f5
rony <armanalicid@gmail.com>
2021-05-17 20:00:51 +0530

    AWS configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Welcome to Amplify Project.</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Welcome to Amplify Project.</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
